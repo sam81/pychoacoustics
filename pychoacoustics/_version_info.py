0185de29d0ea7ad02fd2ac041092a92e62fc9c17
Samuele Carcagno <sam.carcagno@gmail.com>
2013-09-19 10:56:44 +0200

    New release

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.2.56"
-pychoacoustics_builddate = "16-Sep-2013 18:43"
+pychoacoustics_version = "0.2.57"
+pychoacoustics_builddate = "19-Sep-2013 10:56"
