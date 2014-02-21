f48df2ba00678594a31bbf7e5451aa15bc0937e8
Samuele Carcagno <sam.carcagno@gmail.com>
2014-02-21 15:32:27 +0000

    Fix add README.txt for debian package building 2

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.2.65"
-pychoacoustics_builddate = "21-Feb-2014 15:30"
+pychoacoustics_version = "0.2.66"
+pychoacoustics_builddate = "21-Feb-2014 15:32"
