ab35d7aba8c7be2d813fc9e5c1ced9858823fed7
Samuele Carcagno <sam.carcagno@gmail.com>
2015-05-13 16:51:51 +0100

    new release, added UML and psy procedures

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.2.89"
-pychoacoustics_builddate = "21-Nov-2014 16:49"
+pychoacoustics_version = "0.2.90"
+pychoacoustics_builddate = "13-May-2015 16:51"
