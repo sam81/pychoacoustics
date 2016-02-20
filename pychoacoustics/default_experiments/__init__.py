8f8f9d24cfc843b4f81f34468b9944aade0ff993
Samuele Carcagno <sam.carcagno@gmail.com>
2016-02-20 13:05:15 +0000

    Attempt to fix readthedocs error 2

--- pychoacoustics/default_experiments/__init__.py
+++ pychoacoustics/default_experiments/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-__all__ = ["audiogram",
-           "audiogram_mf",
-           "freq",
-           "sig_detect",
-           "F0DL",
-           "level_discrimination",
-           "wav_comparison",
-           "wav_sameDifferent",
-           "wav_ABX",
-           "dummy_adaptive"]
+__all__ = ["audiogram"]# ,
+           # "audiogram_mf",
+           # "freq",
+           # "sig_detect",
+           # "F0DL",
+           # "level_discrimination",
+           # "wav_comparison",
+           # "wav_sameDifferent",
+           # "wav_ABX",
+           # "dummy_adaptive"]
