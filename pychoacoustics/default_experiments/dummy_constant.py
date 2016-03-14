7a59026c57a7b3441a0731e96139c7d2f0682339
Samuele Carcagno <sam.carcagno@gmail.com>
2016-03-14 00:10:38 +0000

    added constant dummy experiments

---
+++ pychoacoustics/default_experiments/dummy_constant.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+"""
+Dummy experiment for several constant paradigms.
+
+"""
+
+from __future__ import nested_scopes, generators, division, absolute_import, with_statement, print_function, unicode_literals
+#from PyQt4 import QtGui, QtCore
+#from PyQt4.QtGui import QApplication
+import random, sys
+from .._version_info import*
+from pychoacoustics.sndlib import*
+                                                                                         
+
+def initialize_dummy_constant(prm):
+    exp_name = "Dummy Constant 1-Interval 2-Alternatives"
+    prm["experimentsChoices"].append(exp_name)
+    prm[exp_name] = {}
+    prm[exp_name]["paradigmChoices"] = ["Constant 1-Interval 2-Alternatives",
+                                        "Constant 1-Pair Same/Different"]
+
+    prm[exp_name]["opts"] = ["hasFeedback"]
+
+    prm[exp_name]["buttonLabels"] = ["A", "B"]
+    prm[exp_name]['defaultNIntervals'] = 1
+    prm[exp_name]['defaultNAlternatives'] = 2
+    
+    prm[exp_name]["execString"] = "dummy_constant"
+    prm[exp_name]["version"] = __name__ + ' ' + pychoacoustics_version + ' ' + pychoacoustics_builddate
+    
+    return prm
+
+def select_default_parameters_dummy_constant(parent, par):
+   
+    field = []
+    fieldLabel = []
+    chooser = []
+    chooserLabel = []
+    chooserOptions = []
+
+    fieldLabel.append(parent.tr("Constant"))
+    field.append(50)
+    
+
+    prm = {}
+    prm['field'] = field
+    prm['fieldLabel'] = fieldLabel
+    prm['chooser'] = chooser
+    prm['chooserLabel'] = chooserLabel
+    prm['chooserOptions'] =  chooserOptions
+
+    return prm
+
+
+def doTrial_dummy_constant(parent):
+  
+    currBlock = 'b'+ str(parent.prm['currentBlock'])
+    if parent.prm['startOfBlock'] == True:
+        parent.writeResultsHeader('log')
+        parent.prm['conditions'] = ["A","B"]
+
+    parent.currentCondition = random.choice(parent.prm['conditions'])
+    if parent.currentCondition == 'A':
+        parent.correctButton = 1
+    elif parent.currentCondition == 'B':
+        parent.correctButton = 2
+
+
+    lev    = parent.prm[currBlock]['field'][parent.prm['fieldLabel'].index("Constant")]
+
+
+ 
+ 
+   
---
+++ tests/test_multiple_constants_m_intervals_n_alternatives.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python 
+# -*- coding: utf-8 -*-
+import numpy, os, sys, unittest
+import pandas as pd
+from test_utility_functions import*
+
+rootPath = "../../pychoacoustics_data/test_data/"
+
+class TestMultiple_ConstantsMIntervalsNAlternatives(unittest.TestCase):
+
+    def testA(self):
+        resFileRoot = "res"
+        removePreviousResFiles(rootPath + "multiple_constants_m-intervals_n-alternatives/"+resFileRoot)
+
+        cmdStr = "python3 ../pychoacoustics.pyw -f" + rootPath +  "multiple_constants_m-intervals_n-alternatives/multiple_constants_m-intervals_n-alternatives.prm -a -q -o --seed 1933 -r" + rootPath + "multiple_constants_m-intervals_n-alternatives/"+resFileRoot  + ".txt"
+        x = os.system(cmdStr)
+        assert(x==0)
+        storedRes = pd.read_csv(rootPath + "multiple_constants_m-intervals_n-alternatives/results/res_table.csv", sep=";")
+        currRes = pd.read_csv(rootPath + "multiple_constants_m-intervals_n-alternatives/res_table.csv", sep=";")
+        numpy.testing.assert_array_equal(currRes["dprime_subc1"], storedRes["dprime_subc1"])
+        numpy.testing.assert_array_equal(currRes["perc_corr_subc1"], storedRes["perc_corr_subc1"])
+        numpy.testing.assert_array_equal(currRes["dprime_subc2"], storedRes["dprime_subc2"])
+        numpy.testing.assert_array_equal(currRes["perc_corr_subc2"], storedRes["perc_corr_subc2"])
+
+        storedResProc = pd.read_csv(rootPath + "multiple_constants_m-intervals_n-alternatives/results/res_table_sess.csv", sep=";")
+        currResProc = pd.read_csv(rootPath + "multiple_constants_m-intervals_n-alternatives/res_table_sess.csv", sep=";")
+
+        numpy.testing.assert_array_equal(currResProc["dprime_subc1"], storedResProc["dprime_subc1"])
+        numpy.testing.assert_array_equal(currResProc["perc_corr_subc1"], storedResProc["perc_corr_subc1"])
+        numpy.testing.assert_array_equal(currResProc["dprime_subc2"], storedResProc["dprime_subc2"])
+        numpy.testing.assert_array_equal(currResProc["perc_corr_subc2"], storedResProc["perc_corr_subc2"])
+
+
+if __name__ == '__main__':
+    unittest.main()
