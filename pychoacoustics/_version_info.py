0c3507067e9f616acfb15ea4c345ee037c469d31
Samuele Carcagno <sam.carcagno@gmail.com>
2019-02-24 10:50:19 +0000

    embed nnresample module

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.5.2"
-pychoacoustics_builddate = "24-Feb-2019 10:49"
+pychoacoustics_version = "0.5.3"
+pychoacoustics_builddate = "24-Feb-2019 10:50"
