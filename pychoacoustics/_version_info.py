8953f728e948aff8400339df82673770e978a3eb
Samuele Carcagno <sam.carcagno@gmail.com>
2019-02-24 11:21:44 +0000

    update manifest.in to include nnresample and subdir

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.5.6"
-pychoacoustics_builddate = "24-Feb-2019 11:16"
+pychoacoustics_version = "0.5.7"
+pychoacoustics_builddate = "24-Feb-2019 11:21"
