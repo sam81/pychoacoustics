0f6aadadb67cd5b514cad5f471324d51d6b65072
Samuele Carcagno <sam.carcagno@gmail.com>
2019-02-24 11:27:11 +0000

    try again fixing nnresample relative import

--- pychoacoustics/_version_info.py
+++ pychoacoustics/_version_info.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-pychoacoustics_version = "0.5.7"
-pychoacoustics_builddate = "24-Feb-2019 11:21"
+pychoacoustics_version = "0.5.8"
+pychoacoustics_builddate = "24-Feb-2019 11:27"
--- setup-pyqt4.py
+++ setup-pyqt4.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 import glob
 setup(
     name="pychoacoustics-pyqt4",
-    version="0.5.7",
+    version="0.5.8",
     url="http://samcarcagno.altervista.org/pychoacoustics/pychoacoustics.html",
     author="Samuele Carcagno",
     author_email="sam.carcagno@google.com;",
